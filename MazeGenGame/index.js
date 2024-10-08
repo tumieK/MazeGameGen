5ccbb7ae02e7a856210aa8ce56e065ac0f663dc2
tumie <151614809+tumieK@users.noreply.github.com>
2024-10-09 01:16:29 +0200

    Update index.js

--- MazeGenGame/index.js
+++ MazeGenGame/index.js
@@ -1,45 +1,22 @@
 const express = require('express')
 const generateMaze = require('./maze');
-
 const app = express();
 const port = 3030;
-
+//Serve static files from the public directory
 app.use(express.static('public'));
-
+//Set the default board and size and initialize the lobbies object to store game session
 const defaultBoardSize = 12;
-
-/**
- * @type {Object.<string, {
- *      id: string,
- *      status: 'waiting' | 'playing' | 'finished',
- *      gravityAngle: number,
- *      winner: string,
- *      
- *      boardSize: number,
- *      walls: [{t: boolean, l: boolean}],
- *      players: Object.<string, {
- *          playerId: string,
- *          username: string,
- *          score: number,
- *          gravityAngle: number,
- *          x: number, y: number,
- *          vx: number, vy: number,
- *          lastPoll: number,
- *          score: number,
- *      }>,
- *      powerUps: [{x: number, y: number, skill: '0g', holder: string, timeActivated: number}]
- * }>}
- */
 let lobbies = {};
-
+//Routing to create a new lobby
 app.get("/lobby/create", (req, res) => {
+    //Below code creates the lobby ID using random 6 letters using uppercase alphabets
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let id = "";
     for (let i = 0; i < 6; i++) {
         let idx = Math.max(Math.min(Math.floor(Math.random()*alphabet.length), alphabet.length), 0);
         id += alphabet[idx];
     }
-
+    // Define a new lobby object with default settings and a randomly generated maze
     let lobby = {
         id,
         status: 'waiting',
@@ -65,7 +42,6 @@ app.get("/lobby/create", (req, res) => {
             i -= 1;
             break;
         }
-
         lobby.powerUps.push({
             x,
             y,
@@ -74,16 +50,12 @@ app.get("/lobby/create", (req, res) => {
             timeActivated: -1,
         });
     }
-
     lobbies[id] = lobby;
-
     res.json(lobby);
 });
-
 app.get("/lobby/join", (req, res) => {
     let lobbyId = req.query.lobby;
     let username_ = req.query.username; // Add to the client
-
     let lobby = lobbies[lobbyId];
     if (lobby === undefined) {
         res.json({'error': "Lobby doesn't exist."});
@@ -100,7 +72,6 @@ app.get("/lobby/join", (req, res) => {
             y: 0,
             vx: 0,
             vy: 0,
-
             lastPoll: new Date().getTime() + 10000,
         };
         res.json({
@@ -109,7 +80,6 @@ app.get("/lobby/join", (req, res) => {
         });
     }
 });
-
 app.get("/lobby/start", (req, res) => {
     let lobbyId = req.query.lobby;
     let lobby = lobbies[lobbyId];
@@ -122,7 +92,6 @@ app.get("/lobby/start", (req, res) => {
         });
     }
 });
-
 app.get("/lobby/reset", (req, res) => {
     let lobbyId = req.query.lobby;
     let lobby = lobbies[lobbyId];
@@ -138,18 +107,14 @@ app.get("/lobby/reset", (req, res) => {
             let idx = lobby.boardSize + j*(lobby.boardSize+1);
             lobby.walls[idx].t = false;
         }
-
         lobby.powerUps = [];
-
         for (let i = 0; i < lobby.boardSize * lobby.boardSize / 32; i++) {
             let x = Math.max(Math.min(Math.floor(Math.random() * lobby.boardSize), lobby.boardSize), 0);
             let y = Math.max(Math.min(Math.floor(Math.random() * lobby.boardSize), lobby.boardSize), 0);
-    
             if (lobby.powerUps.find(value => value.x == x && value.y == y)) {
                 i -= 1;
                 break;
             }
-    
             lobby.powerUps.push({
                 x,
                 y,
@@ -158,7 +123,6 @@ app.get("/lobby/reset", (req, res) => {
                 timeActivated: -1,
             });
         }
-        
         res.json({
             lobby
         });
@@ -198,21 +162,16 @@ app.get("/lobby/poll", (req, res) => {
     const vx = playerId ? parseFloat(req.query.vx) : 0.0;
     const vy = playerId ? parseFloat(req.query.vy) : 0.0;
     const win = req.query.win !== undefined;
-    
     let lobby = lobbies[lobbyId];
-    
     if (lobby === undefined) {
         res.json({'error': "Lobby doesn't exist."});
         return;
     }
-
     if (playerId && !(playerId in lobby.players)) {
         res.json({'error': "Player not in game."});
         return;
     }
-
     const time = new Date().getTime();
-
     if (playerId && timestamp > lobby.players[playerId].lastPoll) {
         lobby.players[playerId].gravityAngle = gravity;
         lobby.players[playerId].x = bx;
@@ -220,30 +179,25 @@ app.get("/lobby/poll", (req, res) => {
         lobby.players[playerId].vx = vx;
         lobby.players[playerId].vy = vy;
         lobby.players[playerId].lastPoll = timestamp;
-
         if (lobby.status === 'playing' && win && !lobby.winner) {
             lobby.winner = playerId;
             lobby.status = 'finished';
             lobby.players[playerId].score += 1;
         }
-    
         let averageGravity = 0.0;
         const keys = Object.keys(lobby.players);
         keys.forEach(p => {
             averageGravity += lobby.players[p].gravityAngle;
         });
         averageGravity /= keys.length || 1;
-    
         lobby.gravityAngle = lobby.gravityAngle * 4 / 5.0 + averageGravity / 5.0;
     }
 
     lobby.powerUps = lobby.powerUps.filter(powerUp => powerUp.timeActivated < 0 || powerUp.timeActivated + 7500 >= time);
-
     let playerRemove = Object.values(lobby.players).filter(player => player.lastPoll >= 0 && player.lastPoll + 10000 < time);
     playerRemove.forEach(p => {
         delete lobby.players[p.playerId];
     });
-
     res.json({
         timestamp: new Date().getTime(),
         lobby,
