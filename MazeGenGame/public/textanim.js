8708545af72a9a306109535e6cb439f2dad83ba2
tumie <151614809+tumieK@users.noreply.github.com>
2024-10-09 22:31:49 +0200

    Update textanim.js

--- MazeGenGame/public/textanim.js
+++ MazeGenGame/public/textanim.js
@@ -1,13 +1,13 @@
-<!-- The UpdateText function targets the HTML element with Id aninimated.The text in the 
-element will be animated -->
+/* The UpdateText function targets the HTML element with Id aninimated.The text in the 
+element will be animated */
 function updateText(text){
 
     let delay = 200;
     
     let h1 = document.getElementById("animated");
-<!-- Here i am splitting the name of the game Maze Danger into individual letters,and wrapp
+/* Here i am splitting the name of the game Maze Danger into individual letters,and wrapp
 each letter into a span element which will allow animation to be applied to each letter,
-    and lastly join the elements into one string -->
+    and lastly join the elements into one string */
         h1.innerHTML = text
         .split("")
         .map(letter => {
@@ -15,9 +15,9 @@ each letter into a span element which will allow animation to be applied to each
             return `<span>` + letter + `</span>`;
         })
         .join("");
-<!-- Here basically i convert each span element wrapping each letter into an array,
+/* Here basically i convert each span element wrapping each letter into an array,
     to be able to iterate through them.And also add a wavy class to each letter,which adds
-a wave like movement to text. -->
+a wave like movement to text. */
         Array.from(h1.children).forEach((span, index) => {
         setTimeout(() => {
             span.classList.add("wavy");
@@ -25,8 +25,8 @@ a wave like movement to text. -->
         });
 
 }
-<!-- This eventLister it is responsible for calling updateText function everytime a page is loadded,
-    this is the text which will be animated and displayed on the first page of the game -->
+/* This eventLister it is responsible for calling updateText function everytime a page is loadded,
+    this is the text which will be animated and displayed on the first page of the game */
 window.addEventListener('load', function () {
     updateText("Maze Danger");
   })
